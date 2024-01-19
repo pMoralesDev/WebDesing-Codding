f207b523b915fae5aa6f25e701869557eaa5f4db
pMoralesDev <pMoralesDev@gmail.com>
2024-01-19 13:02:42 +0100

    24-01-19/#4AdminDasboard-Working

---
+++ #04 - Admin Dashboard/orders.js
@@ -0,0 +1,22 @@
+const Orders = [
+    {
+        productName: 'Java for DAM Students',
+        productNumber: '456721',
+        paymentStatus: 'Due',
+        status: 'Active'
+    },
+
+    {
+        productName: 'Python for Beginners',
+        productNumber: '421630',
+        paymentStatus: 'Paid',
+        status: 'Unactive'
+    },
+
+    {
+        productName: 'Advanced CSS',
+        productNumber: '685002',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    }
+]
